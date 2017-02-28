7f8a0a1f4b974f90fff620751064a54b878860c2
Manuel Kraus <manuel@kraus.rocks>
2017-02-28 16:03:17 +0100

    Utilize moments methods to handle month changes

--- src/Updater.js
+++ src/Updater.js
@@ -56,11 +56,11 @@ class Updater {
     const date = moment();
     const vplanDate = moment(vplan.date);
 
-    if (vplanDate.date() === date.date()) {
+    if (vplanDate.isSame(date)) {
       return this.handleToday(vplan, date);
     }
 
-    if (vplanDate.date() >= date.date()) {
+    if (vplanDate.isAfter(date)) {
       return this.handleTomorrow(vplan, date);
     }
 
