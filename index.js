937f1c319ea507f7c97872ecbb24fc6761a5f0b2
Manuel Kraus <manuel@kraus.rocks>
2017-05-07 17:09:42 +0200

    Use sentry to report uncaught exceptions

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 'use strict';
 
+const Raven = require('raven');
+
 let config;
 try {
 	config = require('./config.json');
-} catch(err) {
+} catch (err) {
 	throw new Error("Couldn't find the configuration file. Please edit config.json according to config.json.example");
 }
+
+Raven.config(config.sentryDNS).install();
 
 global.log = require('./src/util/Logger');
 
